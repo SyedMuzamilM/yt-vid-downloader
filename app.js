3a7c79011e96756e58d7007e1755a74224d69150
Syed Muzamil <smmhd121@gmail.com>
2021-03-15 22:24:12 +0530

    Added body parser

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 /**
  * Import the routes
  */
-const indexRouter = require('./routes/index')
-const downloadRouter = require('./routes/download')
-
+const indexRouter = require("./routes/index");
+const downloadRouter = require("./routes/download");
 
 /**
  * User the routes on the specific endpoints
  */
-app.use('', indexRouter)
-app.use('/download', downloadRouter);
-
-
+app.use("", indexRouter);
+app.use("/download", downloadRouter);
 
 /**
  * Listen of port
  */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`App listning on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`App listning on PORT ${PORT}`));
 
 module.exports = app;
