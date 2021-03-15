fe07a63f54232d6626235fca66aee2ad32acdb93
Syed Muzamil <smmhd121@gmail.com>
2021-03-15 22:26:22 +0530

    Added a new route for the video info page

--- routes/download.js
+++ routes/download.js
@@ -5,6 +5,7 @@ const download_controller = require('../controllers/downloadController')
 
 /* Get the index route */
 downloadRouter.get('/', download_controller.show_download_page)
+downloadRouter.get('/search/video', download_controller.show_video_info)
 downloadRouter.get('/video', download_controller.download_video)
 
 module.exports = downloadRouter;
