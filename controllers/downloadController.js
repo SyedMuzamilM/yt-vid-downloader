522431d0e72c56981876d7e23d4161ba46860523
Syed Muzamil <38078427+SyedMuzamilM@users.noreply.github.com>
2021-08-27 12:44:35 +0530

    Updated the file with try catch

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -15,68 +15,74 @@ exports.show_download_page = (req, res) => {
 
 // This function will run when the use will send the YouTube video link
 exports.show_video_info = async (req, res) => {
-  const ref = req.query.URL;
+  try {
+    const ref = req.query.URL;
 
-  const basic_info = await ytdl.getBasicInfo(ref);
+    const basic_info = await ytdl.getBasicInfo(ref);
 
-  const title = basic_info.videoDetails.title;
-  const vid_id = basic_info.player_response.videoDetails.videoId;
-  const lengthSeconds = parseInt(
-    basic_info.player_response.videoDetails.lengthSeconds
-  );
-  const length = (lengthSeconds / 60).toFixed(2);
-  const thumbnail_length = basic_info.player_response.videoDetails.thumbnail.thumbnails.length
-  const thumbnail_url =
-    basic_info.player_response.videoDetails.thumbnail.thumbnails[thumbnail_length - 1].url;
-  const formats = basic_info.player_response.streamingData.adaptiveFormats;
+    const title = basic_info.videoDetails.title;
+    const vid_id = basic_info.player_response.videoDetails.videoId;
+    const lengthSeconds = parseInt(
+      basic_info.player_response.videoDetails.lengthSeconds
+    );
+    const length = (lengthSeconds / 60).toFixed(2);
+    const thumbnail_length = basic_info.player_response.videoDetails.thumbnail.thumbnails.length
+    const thumbnail_url =
+      basic_info.player_response.videoDetails.thumbnail.thumbnails[thumbnail_length - 1].url;
+    const formats = basic_info.player_response.streamingData.adaptiveFormats;
 
-  let vidQualities = [];
+    let vidQualities = [];
 
-  for (let i = 0; i < formats.length; i++) {
-    // console.log(formats[i])
-    filesize = (parseInt(formats[i].contentLength) / 1024 / 1024).toFixed(2);
-    format_id = formats[i].itag;
-    quality_label = formats[i].qualityLabel;
-    let ext;
-    extension = formats[i].mimeType.slice(6, 10);
-    if (extension.includes(";")) {
-      ext = extension.replace(";", "");
-    } else {
-      ext = extension;
+    for (let i = 0; i < formats.length; i++) {
+      // console.log(formats[i])
+      filesize = (parseInt(formats[i].contentLength) / 1024 / 1024).toFixed(2);
+      format_id = formats[i].itag;
+      quality_label = formats[i].qualityLabel;
+      let ext;
+      extension = formats[i].mimeType.slice(6, 10);
+      if (extension.includes(";")) {
+        ext = extension.replace(";", "");
+      } else {
+        ext = extension;
+      }
+      vidQuality = formats[i].quality;
+      fps = formats[i].fps;
+
+      let newFormat = {
+        filesize,
+        format_id,
+        quality_label,
+        ext,
+        vidQuality,
+        fps,
+      };
+      vidQualities.push(newFormat);
     }
-    vidQuality = formats[i].quality;
-    fps = formats[i].fps;
 
-    let newFormat = {
-      filesize,
-      format_id,
-      quality_label,
-      ext,
-      vidQuality,
-      fps,
-    };
-    vidQualities.push(newFormat);
-  }
+    let qualities_client_want;
+    if (vidQualities.length == 16) {
+      qualities_client_want = vidQualities.slice(0, -8);
+    } else if (vidQualities.length == 14) {
+      qualities_client_want = vidQualities.slice(0, -6);
+    } else {
+      qualities_client_want = vidQualities.slice(0, -10);
+    }
 
-  let qualities_client_want;
-  if (vidQualities.length == 16) {
-    qualities_client_want = vidQualities.slice(0, -8);
-  } else if (vidQualities.length == 14) {
-    qualities_client_want = vidQualities.slice(0, -6);
-  } else {
-    qualities_client_want = vidQualities.slice(0, -10);
+    res.render("video_info", {
+      title,
+      vid_id,
+      length,
+      thumbnail_url,
+      qualities_client_want,
+    });
   }
-  
-  res.render("video_info", {
-    title,
-    vid_id,
-    length,
-    thumbnail_url,
-    qualities_client_want,
-  });
+  catch (err) {
+  console.log(err)
+  }
 };
 
 exports.download_video = async (req, res) => {
+  try {
   const { id, q, title } = req.query;
   const q_id = parseInt(q);
 
@@ -90,7 +96,7 @@ exports.download_video = async (req, res) => {
   audio: { downloaded: 0, total: Infinity },
   video: { downloaded: 0, total: Infinity },
   merged: { frame: 0, speed: '0x', fps: 0 },
-};
+  };
 
 // Get audio and video streams
 const audio = ytdl(id, { quality: 'highestaudio' })
@@ -182,5 +188,9 @@ video.pipe(ffmpegProcess.stdio[5]);
   //   quality: q_id,
   //   format: "mp4"
   // }).pipe(res);
+  }
+  catch (err) {
+    console.log(err) 
+  }
 
 };
